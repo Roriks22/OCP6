4cd7c2ae65690c1b06a37009a5b5c419a32da4b6
romain jacquesson <romain.jacquesson@gmail.com>
2023-11-23 00:13:22 +0100

    ajout_commentaire

--- src/components/Equipment/index.jsx
+++ src/components/Equipment/index.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { useState } from 'react';
 import arrow from '../../assets/images/arrow_back.png';
 
+// Création du composant Equipment et indication des props.
 const Equipment = ({ title, content }) => {
 
+// Utilisation du hook useState.
 const [activeButton, setActiveButton] = useState(false);
 
+// Création d'une fonction permettant avec prevState de retourner à l'état précédent.
 const handleToggle = () => {
     setActiveButton((prevState) => !prevState);
 };
 
     return (
+
+        // Utilisation des className pour gérer le style avec le scss et utilisation de la fonction handleToggle avec le onClick.
         <div className='class_collapse_equipment'>
             <h3 className='collapse_title_equipment' onClick={handleToggle}>
                 {title} 
