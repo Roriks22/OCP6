df6bbd9a2e254333ff248378fbc2adedac95d16d
romain jacquesson <romain.jacquesson@gmail.com>
2023-11-22 18:31:00 +0100

    ajout_commentaire_composant_parent

--- src/index.jsx
+++ src/index.jsx
@@ -13,14 +13,26 @@ import Logement from './pages/Logement';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  <React.StrictMode>
+
+  {/* Utilisation du BrowserRouter pour garder l'interface utilisateur synchronisée avec l'URL. */}
   <BrowserRouter>
+
+  {/* Utilisation du composant Header. */}
   <Header />
+
+  {/* Utilisation du Routes qui permet de coupler segments URL aux composants. */}
   <Routes>
+
+    {/* Utilisation du Route qui permet d'indiquer le composant qui est appélé en fonction de l'URL qui est indiqué. */}
     <Route path="/" element={<Home />} />
     <Route path="/apropos" element={<Apropos />} />
     <Route path="/logement/:id" element={<Logement />} />
+
+    {/* Utilisation de "*" pour gérer toutes les URL qui ne sont pas encore indiquée et appeler composant Error. */}
     <Route path="*" element={<Error />} />
   </Routes>
+  
+  {/* Utilisaiton du composant Footer. */}
   <Footer />
   </BrowserRouter>
  </React.StrictMode>
