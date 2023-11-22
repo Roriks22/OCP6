dac1f1c10415beaeccc85299e0ae392b86bd1e42
romain jacquesson <romain.jacquesson@gmail.com>
2023-11-23 00:24:05 +0100

    ajout_commentaire

--- src/components/Tag/index.jsx
+++ src/components/Tag/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
+// Création du composant Tag et indication des props.
 const Tag = ({ tagName }) => {
     return (
+        
+        // Utilisation du className pour gérer le style scss des tags.
         <p className="tag_name">{tagName}</p>
     );
 };
