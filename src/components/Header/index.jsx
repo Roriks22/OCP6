acbdb54c5bf6deae0dca9937ebac575351ff812e
romain jacquesson <romain.jacquesson@gmail.com>
2023-11-23 00:35:07 +0100

    ajout_commentaire

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import logo from '../../assets/images/logo.png';
 import { Link, NavLink } from 'react-router-dom';
 
+// Création du composant Header.
 const Header = () => {
     return (
         <header>
+
+            {/* Utilisation du Link utilisable avec react-router-dom pour gérer le lien ainsi que des className pour gérer le style scss du logo. */}
             <Link to="/">
                 <img className='logo_header' src={logo} alt="logo Kasa" />
             </Link>
             <nav>
                 <ul>
+                    
+                    {/* Utilisation du NavLink utilisable avec react-router-dom qui permet de créer des liens et qui permet également de gérer l'état des liens. */}
                     <li><NavLink className="style_header" to="/" style={({ isActive}) => ({textDecoration: isActive ? "underline" : "none" })}>Accueil</NavLink></li>
                     <li><NavLink className="style_header" to="apropos" style={({ isActive }) => ({ textDecoration: isActive ? "underline" : "none" })}>A Propos</NavLink></li>
                 </ul>
