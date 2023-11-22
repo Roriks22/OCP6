54c63e3b87b126d85120f6c944ccd0e556398749
romain jacquesson <romain.jacquesson@gmail.com>
2023-11-23 00:27:27 +0100

    ajout_commentaire

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import logo_footer from '../../assets/images/logo_footer.png';
 
+// Création du composant Footer.
 const Footer = () => {
     return (
         <footer>
+            
+            {/* Mis en place de l'image du footer, du alt et de la description */}
             <img src={logo_footer} alt="logo footer Kasa" />
             <p>© 2020 Kasa. All rights reserved</p>
         </footer>
