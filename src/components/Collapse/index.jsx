3973b3461c9e97507139f3902b2c438fb7d58512
romain jacquesson <romain.jacquesson@gmail.com>
2023-11-23 00:09:59 +0100

    ajout_commentaire

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { useState } from 'react';
 import arrow from '../../assets/images/arrow_back.png';
 
+// Création du composant Collapse et indication des props.
 const Collapse = ({ title, content }) => {
 
+// Utilisation du hook useState.
 const [activeButton, setActiveButton] = useState(false);
 
+// Création d'une fonction permettant avec prevState de retourner à l'état précédent.
 const handleToggle = () => {
     setActiveButton((prevState) => !prevState);
 };
 
     return (
+
+        // Utilisation des className pour gérer le style avec le scss et utilisation de la fonction handleToggle avec le onClick.
         <div className='class_collapse'>
             <h3 className='collapse_title' onClick={handleToggle}>
                 {title} 
