7aa1300c06c4447a68a6cd4ce037b953783ecc9b
romain jacquesson <romain.jacquesson@gmail.com>
2023-11-23 00:22:09 +0100

    ajout_commentaire

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import etoile_rose from '../../assets/images/star-active.png';
 import etoile_grise from '../../assets/images/star-inactive.png';
 
+// Création du composant Rating et indication des props.
 const Rating = ({ rating }) => {
+    
+    // Utilisation d'un Array pour gérer le nombre d'étoiles possible et l'utilisation d'un map() pour gérer l'apparation des étoiles roses et/ou grises.
 const etoileImage = Array.from({ length : 5}).map((_,index) => (
     <img key={index} src={index < rating ? etoile_rose : etoile_grise} alt="etoile" />
 ));
 
 return (
+
+    // Utilisation du className pour gérer le style en scss du rating.
 <div className='rating'>{etoileImage}</div>
 );
 }
