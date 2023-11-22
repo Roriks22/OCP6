b9ca6bdbac85a12420b5eeefa004f540d21fc2b8
romain jacquesson <romain.jacquesson@gmail.com>
2023-11-22 18:18:56 +0100

    ajout_commentaire_page_error

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -3,11 +3,17 @@ import error_404 from '../../assets/images/404.png';
 
 const Error = () => {
     return (
+
+        // Utilisation du className pour gérer le style avec le scss.
         <div className='error'>
             <h1>
+
+                {/* Utilisation de l'img pour mettre l'image error 404. */}
                 <img src={error_404} alt="image_404"></img>
             </h1>
             <h2>Oups! La page que vous demandez n'existe pas.</h2>
+
+            {/* Faire un lien pour retourner à la page Home. */}
             <a href="/">Retourner sur la page d'accueil</a>
         </div>
     );
