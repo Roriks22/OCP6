258ee850a9d5e0e4950fe3e516001576ffc2e1d4
romain jacquesson <romain.jacquesson@gmail.com>
2023-11-22 18:36:54 +0100

    ajout_commentaire_composant_banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
+// Création de la fonction Banner avec les props.
 const Banner = ({imgSrc, imgText, imgAlt}) => {
     return (
+
+        // Ajout d'un className pour gérer le style scss.
         <div className='banner'>
+
+            {/* Ajout d'un titre et d'une img que l'on gèrera plus tard grâce aux props. */}
             <h2>{imgText}</h2>
             <img src={imgSrc} alt={imgAlt} />
         </div>    
