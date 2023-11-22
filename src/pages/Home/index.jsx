0f0844590accbcbdceba55ef48710c9cf6ae115e
romain jacquesson <romain.jacquesson@gmail.com>
2023-11-22 18:14:46 +0100

    ajout-commentaire_page_home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,15 +6,22 @@ import { logements } from '../../datas/logements';
 
 const Home = () => {
     return (
+        // Utilisation de l'id pour gérer avec le style scss.
     <div id="home">
         <div id="banner_ChezVous">
+
+            {/* Utilisation du composant Banner avec les props pour pouvoir mettre l'image, le text et le alt. */}
             <Banner
             imgSrc={banner_home}
             imgText="Chez vous, partout et ailleurs"
             imgAlt="Paysage" />
         </div>
+
+        {/* Utilisation de l'id pour gérer le style des logements et utilisation de map() pour pouvoir gérer chaque élement de la data des logements. */}
         <div id="logements">
             {logements.map((logement) => {
+                
+                // Utilisation du composant enfant Card pour pour pouvoir gérer avec les props et la data Logements.
                 return <Card key={logement.id} id={logement.id} title={logement.title} image={logement.cover} />
             })}
         </div>
